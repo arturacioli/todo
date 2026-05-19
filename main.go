5f54471383e85be17203ad241b362b1803eb1c69
Artur Acioli <arturacioli505@gmail.com>
2026-05-19 16:15:00 -0300

    add: initializing basic project architecture

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Cfg = struct{
+	PathToCsv string	
+}
+
+type Command struct{
+	Handler func(args []string)
+}
+
+type Cli struct{
+	Commands  map[string]Command
+	Arguments []string
+	Cfg       *Cfg
+}
+
+func (cli *Cli)AddCommand(name string,
+				handler func(args []string)){
+	
+	newCmd := Command{
+		Handler: handler,
+	}
+	cli.Commands[name] = newCmd
+}
+
+func (cli *Cli)ExecuteCommand(commandName string) error{
+	if _,ok := cli.Commands[commandName]; !ok{
+		return fmt.Errorf("Command not mapped")
+	}
+
+	cli.Commands[commandName].Handler(cli.Arguments)
+	return nil
+}
+
+func main(){
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: <command> [args]")
+		os.Exit(1)
+	}
+	cfg := Cfg{
+		PathToCsv: "",
+	}
+	cli := Cli{
+		Commands: make(map[string]Command),
+		Arguments: os.Args[2:],
+		Cfg: &cfg,
+	}
+
+
+	cli.AddCommand("add",testeAdd)
+
+	
+	command := os.Args[1]
+	cli.ExecuteCommand(command)
+}
+
+func testeAdd(args []string){
+	fmt.Print("add funcionando")
+}
+
+
